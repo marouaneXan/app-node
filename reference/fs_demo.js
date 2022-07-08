d473aaa6f63995400f207b0e877e68f43246ed8f
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-08 14:17:13 +0100

    create a folder with mkdir

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,3 +2,9 @@ const fs = require('fs')
 const path = require('path')
 
 //Create folder
+fs.mkdir(path.join(__dirname,'/test'),{},err => {
+    if (err) throw err;
+    console.log('folder created successfully');
+});
+
+//Create and write into the file
